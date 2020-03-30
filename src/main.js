c2da7088c7ced5c6b280ced0fd5d5e6262a20513
numsi <1406102869@qq.com>
2020-03-30 14:14:28 +0800

    必要组件安装和项目初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+import axios from 'axios'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+
+//组件使用
+Vue.use(mavonEditor)
+Vuu.use(Element)
+//全局注册组件
+Vue.prototype.axios = axios;
 
 Vue.config.productionTip = false
 
